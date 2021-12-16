2c4f7f50f4b2ac21a122f8c8747982a7452047fa
victor pena <victor.pena1392@Gmail.com>
2021-12-16 17:18:05 -0600

    added an object of coffees and created a function to add new coffees

---
+++ js/main.js
@@ -0,0 +1,74 @@
+'use strict'
+
+var coffeeList = [
+    {
+        name: 'Light City',
+        roast: 'Light'
+    },
+    {
+        name: 'Half City',
+        roast: 'Light'
+    },
+    {
+        name: 'Cinnamon',
+        roast: 'Light'
+    },
+    {
+        name: 'City',
+        roast: 'Medium'
+    },
+    {
+        name: 'American',
+        roast: 'Medium'
+    },
+    {
+        name: 'Breakfast',
+        roast: 'Medium'
+    },
+    {
+        name: 'High',
+        roast: 'Dark',
+    },
+    {
+        name: 'Continental',
+        roast: 'Dark'
+    },
+    {
+        name: 'New Orleans',
+        roast: 'Dark'
+    },
+    {
+        name: 'European',
+        roast: 'Dark'
+    },
+    {
+        name: 'Espresso',
+        roast: 'Dark'
+    },
+    {
+        name: 'Viennese',
+        roast: 'Dark'
+    },
+    {
+        name: 'Italian',
+        roast: 'Dark'
+    },
+    {
+        name: 'French',
+        roast: 'Dark'
+    }
+]
+
+function addCoffee(name, roast) {
+    var coffee = [
+        {
+            name: name,
+            roast: roast
+
+        }
+    ]
+    coffeeList.push(coffee);
+}
+
+
+
