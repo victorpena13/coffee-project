00577bb16a4fb597f1470c8be01593dad87c829d
victor pena <victor.pena1392@Gmail.com>
2021-12-16 18:24:14 -0600

    used a forEach loop to iterate through the coffeeList object and display the names of each coffee

--- js/main.js
+++ js/main.js
@@ -1,62 +1,20 @@
 'use strict'
 
 var coffeeList = [
-    {
-        name: 'Light City',
-        roast: 'Light'
-    },
-    {
-        name: 'Half City',
-        roast: 'Light'
-    },
-    {
-        name: 'Cinnamon',
-        roast: 'Light'
-    },
-    {
-        name: 'City',
-        roast: 'Medium'
-    },
-    {
-        name: 'American',
-        roast: 'Medium'
-    },
-    {
-        name: 'Breakfast',
-        roast: 'Medium'
-    },
-    {
-        name: 'High',
-        roast: 'Dark',
-    },
-    {
-        name: 'Continental',
-        roast: 'Dark'
-    },
-    {
-        name: 'New Orleans',
-        roast: 'Dark'
-    },
-    {
-        name: 'European',
-        roast: 'Dark'
-    },
-    {
-        name: 'Espresso',
-        roast: 'Dark'
-    },
-    {
-        name: 'Viennese',
-        roast: 'Dark'
-    },
-    {
-        name: 'Italian',
-        roast: 'Dark'
-    },
-    {
-        name: 'French',
-        roast: 'Dark'
-    }
+    {name: 'Light City', roast: 'Light'},
+    {name: 'Half City', roast: 'Light'},
+    {name: 'Cinnamon', roast: 'Light'},
+    {name: 'City', roast: 'Medium'},
+    {name: 'American', roast: 'Medium'},
+    {name: 'Breakfast', roast: 'Medium'},
+    {name: 'High', roast: 'Dark',},
+    {name: 'Continental', roast: 'Dark'},
+    {name: 'New Orleans', roast: 'Dark'},
+    {name: 'European', roast: 'Dark'},
+    {name: 'Espresso', roast: 'Dark'},
+    {name: 'Viennese', roast: 'Dark'},
+    {name: 'Italian', roast: 'Dark'},
+    {name: 'French', roast: 'Dark'}
 ]
 
 function addCoffee(name, roast) {
@@ -69,6 +27,14 @@ function addCoffee(name, roast) {
     ]
     coffeeList.push(coffee);
 }
+function displayCoffee() {
+    var html = '';
+    coffeeList.forEach(function(coffee){
+        html += coffee.name + "\n";
+    });
+    return html;
+}
 
 
+document.getElementById('coffees-object').innerText = displayCoffee();
 
